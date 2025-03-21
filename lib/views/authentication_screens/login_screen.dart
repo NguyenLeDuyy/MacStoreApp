438eecac34821033b3e0b3f9d5e2d2df1456a834
Nguyễn Lê Duy <147262594+NguyenLeDuyy@users.noreply.github.com>
2025-03-21 20:57:41 +0700

    add a images from assets that have just configued in pubspec.yaml file

--- lib/views/authentication_screens/login_screen.dart
+++ lib/views/authentication_screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -6,11 +7,37 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text("Login Your Account"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Login Your Account",
+              style: GoogleFonts.getFont(
+                'Lato',
+                color: Color(0xFF0d120E),
+                fontWeight: FontWeight.bold,
+                letterSpacing: 0.2,
+                fontSize: 22,
+              ),
+            ),
+            Text(
+              'To Explore the world exclusives',
+              style: GoogleFonts.getFont(
+                'Lato',
+                color: Color(0xFF0d120E),
+                fontSize: 14,
+                letterSpacing: 0.2,
+              ),
+            ),
 
-        ],
+            Image.asset(
+              'assets/images/Illustration.png',
+              width: 200,
+              height: 200,
+            ),
+          ],
+        ),
       ),
     );
   }
