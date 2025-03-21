cd7299a7b0a62598abc63721f71de4fbe7665e6a
Nguyễn Lê Duy <147262594+NguyenLeDuyy@users.noreply.github.com>
2025-03-22 00:05:08 +0700

    validate register and login form in UI

--- lib/views/authentication_screens/register_screen.dart
+++ lib/views/authentication_screens/register_screen.dart
@@ -3,7 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:mac_store_app/views/authentication_screens/login_screen.dart';
 
 class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({super.key});
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -12,209 +12,246 @@ class RegisterScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Create Your Account",
-                style: GoogleFonts.getFont(
-                  'Lato',
-                  color: Color(0xFF0d120E),
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 0.2,
-                  fontSize: 22,
-                ),
-              ),
-              Text(
-                'To Explore the world exclusives',
-                style: GoogleFonts.getFont(
-                  'Lato',
-                  color: Color(0xFF0d120E),
-                  fontSize: 14,
-                  letterSpacing: 0.2,
-                ),
-              ),
-
-              Image.asset(
-                'assets/images/Illustration.png',
-                width: 200,
-                height: 200,
-              ),
-
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'Full name',
-                  style: GoogleFonts.getFont(
-                    'Nunito Sans',
-                    fontWeight: FontWeight.w600,
-                    letterSpacing: 0.2,
-                  ),
-                ),
-              ),
-
-              TextFormField(
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(9),
-                  ),
-                  focusedBorder: InputBorder.none,
-                  enabledBorder: InputBorder.none,
-                  labelText: "Enter your full name",
-                  labelStyle: GoogleFonts.getFont(
-                    "Nunito Sans",
-                    fontSize: 14,
-                    letterSpacing: 0.1,
-                  ),
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Image.asset(
-                      'assets/icons/user.jpeg',
-                      width: 20,
-                      height: 20,
-                    ),
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 20),
-
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'Email',
-                  style: GoogleFonts.getFont(
-                    'Nunito Sans',
-                    fontWeight: FontWeight.w600,
-                    letterSpacing: 0.2,
-                  ),
-                ),
-              ),
-
-              TextFormField(
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(9),
-                  ),
-                  focusedBorder: InputBorder.none,
-                  enabledBorder: InputBorder.none,
-                  labelText: "Enter your email",
-                  labelStyle: GoogleFonts.getFont(
-                    "Nunito Sans",
-                    fontSize: 14,
-                    letterSpacing: 0.1,
-                  ),
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Image.asset(
-                      'assets/icons/password.png',
-                      width: 20,
-                      height: 20,
-                    ),
-                  ),
-
-                  suffixIcon: Icon(Icons.visibility),
-                ),
-              ),
-
-              SizedBox(height: 20),
-
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'Password',
-                  style: GoogleFonts.getFont(
-                    'Nunito Sans',
-                    fontWeight: FontWeight.w600,
-                    letterSpacing: 0.2,
-                  ),
-                ),
-              ),
-
-              TextFormField(
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(9),
-                  ),
-                  focusedBorder: InputBorder.none,
-                  enabledBorder: InputBorder.none,
-                  labelText: "Enter your password",
-                  labelStyle: GoogleFonts.getFont(
-                    "Nunito Sans",
-                    fontSize: 14,
-                    letterSpacing: 0.1,
-                  ),
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Image.asset(
-                      'assets/icons/password.png',
-                      width: 20,
-                      height: 20,
-                    ),
-                  ),
-
-                  suffixIcon: Icon(Icons.visibility),
-                ),
-              ),
-
-              SizedBox(height: 20),
-
-              Container(
-                width: 319,
-                height: 50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  gradient: LinearGradient(
-                    colors: [Color(0xFF102DE1), Color(0xCC0D6EFF)],
-                  ),
-                ),
-                child: Center(
-                  child: Text(
-                    'Sign up',
-                    style: GoogleFonts.getFont(
-                      'Lato',
-                      fontSize: 17,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 20),
-
-              Row(
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    'Already have an account?',
-                    style: GoogleFonts.roboto(
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 1,
+                    "Create Your Account",
+                    style: GoogleFonts.getFont(
+                      'Lato',
+                      color: Color(0xFF0d120E),
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 0.2,
+                      fontSize: 22,
                     ),
                   ),
-                  InkWell(
-                    onTap: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context){
-                        return LoginScreen();
-                      }));
-                    },
+                  Text(
+                    'To Explore the world exclusives',
+                    style: GoogleFonts.getFont(
+                      'Lato',
+                      color: Color(0xFF0d120E),
+                      fontSize: 14,
+                      letterSpacing: 0.2,
+                    ),
+                  ),
+              
+                  Image.asset(
+                    'assets/images/Illustration.png',
+                    width: 200,
+                    height: 200,
+                  ),
+              
+                  Align(
+                    alignment: Alignment.topLeft,
                     child: Text(
-                      'Sign In',
-                      style: GoogleFonts.roboto(
-                        color: Color(0xFF102DE1),
-                        fontWeight: FontWeight.bold,
+                      'Full name',
+                      style: GoogleFonts.getFont(
+                        'Nunito Sans',
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 0.2,
                       ),
                     ),
                   ),
+              
+                  TextFormField(
+                    validator: (value){
+                      if (value!.isEmpty){
+                        return "Enter your email";
+                      }
+                      else {
+                        return null;
+                      }
+                    },
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(9),
+                      ),
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      labelText: "Enter your full name",
+                      labelStyle: GoogleFonts.getFont(
+                        "Nunito Sans",
+                        fontSize: 14,
+                        letterSpacing: 0.1,
+                      ),
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Image.asset(
+                          'assets/icons/user.jpeg',
+                          width: 20,
+                          height: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+              
+                  SizedBox(height: 20),
+              
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Email',
+                      style: GoogleFonts.getFont(
+                        'Nunito Sans',
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 0.2,
+                      ),
+                    ),
+                  ),
+              
+                  TextFormField(
+                    validator: (value){
+                      if(value!.isEmpty) {
+                        return 'Enter your email';
+                      } else {
+                        return null;
+                      }
+                    },
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(9),
+                      ),
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      labelText: "Enter your email",
+                      labelStyle: GoogleFonts.getFont(
+                        "Nunito Sans",
+                        fontSize: 14,
+                        letterSpacing: 0.1,
+                      ),
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Image.asset(
+                          'assets/icons/password.png',
+                          width: 20,
+                          height: 20,
+                        ),
+                      ),
+              
+                      suffixIcon: Icon(Icons.visibility),
+                    ),
+                  ),
+              
+                  SizedBox(height: 20),
+              
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Password',
+                      style: GoogleFonts.getFont(
+                        'Nunito Sans',
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 0.2,
+                      ),
+                    ),
+                  ),
+              
+                  TextFormField(
+                    validator: (value){
+                      if(value!.isEmpty) {
+                        return 'Enter your password';
+                      } else {
+                        return null;
+                      }
+                    },
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(9),
+                      ),
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      labelText: "Enter your password",
+                      labelStyle: GoogleFonts.getFont(
+                        "Nunito Sans",
+                        fontSize: 14,
+                        letterSpacing: 0.1,
+                      ),
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Image.asset(
+                          'assets/icons/password.png',
+                          width: 20,
+                          height: 20,
+                        ),
+                      ),
+              
+                      suffixIcon: Icon(Icons.visibility),
+                    ),
+                  ),
+              
+                  SizedBox(height: 20),
+              
+                  InkWell(
+                    onTap: (){
+                      if (_formKey.currentState!.validate()){
+                        print("correct");
+                      }
+                      else {
+                        print("failed");;
+                      }
+                    },
+                    child: Container(
+                      width: 319,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        gradient: LinearGradient(
+                          colors: [Color(0xFF102DE1), Color(0xCC0D6EFF)],
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Sign up',
+                          style: GoogleFonts.getFont(
+                            'Lato',
+                            fontSize: 17,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+              
+                  SizedBox(height: 20),
+              
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Already have an account?',
+                        style: GoogleFonts.roboto(
+                          fontWeight: FontWeight.w500,
+                          letterSpacing: 1,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context){
+                            return LoginScreen();
+                          }));
+                        },
+                        child: Text(
+                          'Sign In',
+                          style: GoogleFonts.roboto(
+                            color: Color(0xFF102DE1),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
-            ],
+            ),
           ),
         ),
       ),
