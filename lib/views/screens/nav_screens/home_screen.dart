72d60dda9f31911386ca2f670ee9d56b019ea732
PHUONGLINH0901 <dboconganh3@gmail.com>
2025-04-08 19:42:21 +0700

    WorkingonHeaderSection

--- lib/views/screens/nav_screens/home_screen.dart
+++ lib/views/screens/nav_screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mac_store_app/views/screens/nav_screens/widgets/header_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +7,15 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: Text('Home Screen'))
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            HeaderWidget(),
+          ],
+        ),
+      ),
     );
   }
 }
+
+
