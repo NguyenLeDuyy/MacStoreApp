cd86c4821c99cbfb69c801a3c5102a4f0535f38f
NguyenHongLoan125 <2251120364@ut.edu.vn>
2025-04-10 18:50:31 +0700

    Categories State Management

---
+++ lib/controllers/category_controller.dart
@@ -0,0 +1,58 @@
+import 'package:get/get.dart';
+import 'package:mac_store_app/models/category_models.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+//**
+class CategoryController extends GetxController {
+  final SupabaseClient _supabase = Supabase.instance.client;
+
+  RxList<CategoryModel> categories = <CategoryModel>[].obs;
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    _fetchCategories();
+  }
+
+  void _fetchCategories() {
+    _supabase
+        .from('categories')
+        .stream(primaryKey: ['id'])
+        .listen((List<Map<String, dynamic>> data) {
+      categories.assignAll(data.map((doc) {
+        return CategoryModel(
+          category_name: doc['category_name'],
+          category_image: doc['category_image'],
+        );
+      }).toList());
+    });
+  }
+}
+//**
+
+// class CategoryController extends GetxController{
+//   final Supabase _supastore = Supabase.instance;
+//   RxList<CategoryModel> categories = <CategoryModel>[].obs;
+//
+// @override
+//   void onInit() {
+//     // TODO: implement onInit
+//     super.onInit();
+//     _fetchCategories();
+//   }
+//
+//   void _fetchCategories(){
+//     _supastore
+//         .collection('categories')
+//         .snapshots()
+//         .listen((QuerySnapshot querySnapshots){
+//           categories.assignAll(querySnapshots.docs.map((doc) {
+//             final data = doc.data() as Map<String, dynamic>;
+//             return CategoryModel(
+//                 category_name: data['category_name'],
+//                 category_image: data['category_image']);
+//           }).toList(),
+//         );
+//     });
+//   }
+// }
---
+++ lib/models/category_models.dart
@@ -0,0 +1,8 @@
+class CategoryModel{
+  final String category_name;
+  final String category_image;
+
+  CategoryModel({required this.category_name, required this.category_image});
+
+
+}
