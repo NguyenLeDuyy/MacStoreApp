eaa706a87aec429f3104dd3d98cc6e6870c9c610
NguyenHongLoan125 <2251120364@ut.edu.vn>
2025-05-04 02:10:42 +0700

    MainScreen

--- lib/views/screens/nav_screens/widgets/reuseable_text_widget.dart
+++ lib/views/screens/nav_screens/widgets/reuseable_text_widget.dart
@@ -4,38 +4,42 @@ import 'package:google_fonts/google_fonts.dart';
 class ReuseableTextWidget extends StatelessWidget {
   final String title;
   final String subTitle;
+  final VoidCallback? onTap;
 
   const ReuseableTextWidget({
     super.key,
     required this.title,
     required this.subTitle,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            title,
-            style: GoogleFonts.roboto(
-              fontSize: 14,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 1,
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: GoogleFonts.roboto(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1,
+              ),
             ),
-          ),
-
-          Text(
-            subTitle, 
-            style: GoogleFonts.roboto(
-              color: Colors.blue,
-              fontWeight: FontWeight.bold,
-              fontSize: 13,
+            Text(
+              subTitle,
+              style: GoogleFonts.roboto(
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+                fontSize: 13,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
